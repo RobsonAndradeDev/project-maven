3e4a229e46bdb772e6871f92fef63b95b6bf32cd
RobsonAndradeDev <robson_andrade_dev@outlook.com>
2016-11-21 12:38:34 -0200

    change main

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -2,7 +2,8 @@ package project;
 
 public class Main {
   public static void main(String[] args) {
-	  System.out.println("testing maven");
-	  //crash
+	  Maria mariaClass = new Maria();
+	  mariaClass.setName("João");
+	  System.out.println("O nome da maria agora é: " + mariaClass.getName());
   }
 }
